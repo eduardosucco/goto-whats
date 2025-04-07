6742c3f23e9b51bb7a9c220ed6623994c00ec516
Eduardo Succo <eduardo.succo@gmail.com>
2025-04-07 22:24:36 +0000

    Nova tentativa

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
 import streamlit as st
+import streamlit.components.v1 as components
 
 def main():
-    st.title("Abrir Conversa no WhatsApp (via Meta Refresh)")
-    st.write("Digite o número (exemplo: 5511999999999) e clique em abrir.")
+    numero = st.text_input("Número completo", "21981664493")
 
-    numero = st.text_input("Número completo (com DDD)", "11999999999")
-
-    if st.button("Abrir WhatsApp"):
-        st.write("Tentando abrir WhatsApp via meta refresh…")
+    if st.button("Abrir WhatsApp agora!"):
         link_whatsapp = f"https://wa.me/55{numero}"
-        meta_tag = f'<meta http-equiv="refresh" content="0; url={link_whatsapp}"/>'
-        st.markdown(meta_tag, unsafe_allow_html=True)
+        js_code = f"""
+        <script>
+            window.open('{link_whatsapp}', '_self');
+        </script>
+        """
+        st.write("Tentando abrir WhatsApp...")
+        components.html(js_code, height=0)
 
 if __name__ == "__main__":
     main()
