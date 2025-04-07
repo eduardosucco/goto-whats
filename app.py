b242425a3a43ed8e86f2956a9b769bcee859c14f
Eduardo Succo <eduardo.succo@gmail.com>
2025-04-07 22:12:26 +0000

    Att app

--- app.py
+++ app.py
@@ -2,15 +2,14 @@ import streamlit as st
 
 def main():
     st.title("Abrir Chat do WhatsApp")
-    st.write("Clique no botão para abrir a conversa no WhatsApp.")
+    st.write("Insira o número com DDD (ex: 5511999999999) e clique no botão para abrir a conversa:")
 
-    # Exemplo de número fictício
-    numero_completo = "5511999999999"  # +55 11 99999-9999
-    link_whatsapp = f"https://wa.me/{numero_completo}"
+    # Campo de entrada de texto para o número
+    numero_completo = st.text_input("Número completo", value="5511999999999")
 
+    # Quando clica no botão, monta o link e exibe
     if st.button("Abrir WhatsApp"):
-        # Quando o botão for clicado, mostra um link clicável na interface.
-        # Em um ambiente mobile, ao clicar nesse link, vai abrir direto o app do WhatsApp.
+        link_whatsapp = f"https://wa.me/{numero_completo}"
         st.markdown(f"[Clique aqui para iniciar o chat]({link_whatsapp})")
 
 if __name__ == "__main__":
